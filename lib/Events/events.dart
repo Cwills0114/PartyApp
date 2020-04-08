e8035f58f1046e8ecd6cead4223963b299954be5
Cwills0114 <37110561+Cwills0114@users.noreply.github.com>
2020-04-08 23:48:29 +0100

    Added Card view Updates

--- lib/Events/events.dart
+++ lib/Events/events.dart
@@ -2,34 +2,55 @@ import 'package:flutter/material.dart';
 
 /*
   Method: display_Event
-  Description: Build the Card view for
+  Description: Build the Card view for HomePageLayout
 */
 
-Widget display_Event(BuildContext context){
+Widget display_Event(BuildContext context) {
   List items = getDummyList();
-  
-    return Padding(
-      padding: EdgeInsets.only(top: 200),
-      child: ListView.builder(
-        itemCount: items.length, 
-        itemBuilder: (context, index){
-          return Dismissible(key: Key(items[index]),
-           child: Card(
-             elevation: 3,
-             child: Container(
-               height: 50,
-               child: Row(
-                 children: <Widget> [Text(items[index])],
-               ),
-             ),
-           ) );
-        }
-        ),
-    );
-  }
-  
+
+  return Padding(
+    padding: EdgeInsets.only(top: 200),
+    child: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 3,
+            child: Container(
+              height: 125,
+              
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(items[index]),
+
+                      Text(items[index]),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(items[index]),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(items[index]),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        }),
+  );
+}
+
 List getDummyList() {
-  List list = List.generate(20, (i){
+  List list = List.generate(20, (i) {
     return "Item ${i + 1}";
   });
   return list;
